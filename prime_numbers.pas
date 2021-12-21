19ef2d6a43e1c8e9b3ff931e7cb2daf9ea91dbe4
gmankab <gmankab@gmail.com>
2021-12-21 23:45:32 +0300

    aboba

--- prime_numbers.pas
+++ prime_numbers.pas
@@ -1,7 +1,15 @@
 program prime_numbers;
 
-var a:Integer;
- //
+
+function check_if_prime(num:integer):Boolean;
+var i:Integer;
 begin
-  writeln('a')
+  check_if_prime := False;
+  for i := 2 to num - 1 do
+      if num mod i = 0 then
+        check_if_prime := True;
+end;
+
+begin
+  writeln(check_if_prime())
 end.
